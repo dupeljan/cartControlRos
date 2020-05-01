2b53f2109d7358feb357ed0e4d77f4e48ea62e4d
dupel <dupeljan@gmail.com>
2020-05-01 13:59:17 +0300

    Client rotate the cart

--- plugin/client.cc
+++ plugin/client.cc
@@ -25,7 +25,6 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-// %Tag(FULLTEXT)%
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
@@ -33,30 +32,28 @@
 #include <string>
 #include <iostream>
 
+#define TOPIC_NAME "/robot_mobile_wheel_3_omni_open_base/velocity"
 
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
-// %Tag(CALLBACK)%
-void chatterCallback(const CartConrolPlugin::VelocityConstPtr &msg)
-{
-  std::cout << std::to_string(msg->left) + '\n';
+
+std::string velocityToString(const CartConrolPlugin::Velocity v){
+    return  "left " + std::to_string(v.left) +'\n' +
+            "right " + std::to_string(v.right) + '\n' +
+            "back " + std::to_string(v.back) + '\n';
 }
-// %EndTag(CALLBACK)%
-
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
-  ros::init(argc, argv, "listener");
+    /**
+       * The ros::init() function needs to see argc and argv so that it can perform
+       * any ROS arguments and name remapping that were provided at the command line.
+       * For programmatic remappings you can use a different version of init() which takes
+       * remappings directly, but for most command-line programs, passing argc and argv is
+       * the easiest way to do it.  The third argument to init() is the name of the node.
+       *
+       * You must call one of the versions of ros::init() before using any other
+       * part of the ROS system.
+       */
+      ros::init(argc, argv, "talker");
+
 
   /**
    * NodeHandle is the main access point to communications with the ROS system.
@@ -64,35 +61,69 @@ int main(int argc, char **argv)
    * NodeHandle destructed will close down the node.
    */
   ros::NodeHandle n;
-
   /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-// %Tag(SUBSCRIBER)%
-  ros::Subscriber sub = n.subscribe("/robot_mobile_wheel_3_omni_open_base/velocity", 1000, chatterCallback);
-// %EndTag(SUBSCRIBER)%
+     * The advertise() function is how you tell ROS that you want to
+     * publish on a given topic name. This invokes a call to the ROS
+     * master node, which keeps a registry of who is publishing and who
+     * is subscribing. After this advertise() call is made, the master
+     * node will notify anyone who is trying to subscribe to this topic name,
+     * and they will in turn negotiate a peer-to-peer connection with this
+     * node.  advertise() returns a Publisher object which allows you to
+     * publish messages on that topic through a call to publish().  Once
+     * all copies of the returned Publisher object are destroyed, the topic
+     * will be automatically unadvertised.
+     *
+     * The second parameter to advertise() is the size of the message queue
+     * used for publishing messages.  If messages are published more quickly
+     * than we can send them, the number here specifies how many messages to
+     * buffer up before throwing some away.
+     */
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
-// %Tag(SPIN)%
-  ros::spin();
-// %EndTag(SPIN)%
+    ros::Publisher chatter_pub = n.advertise<CartConrolPlugin::Velocity>(TOPIC_NAME, 1000);
+
+
+      ros::Rate loop_rate(10);
+
+
+      /**
+       * A count of how many messages we have sent. This is used to create
+       * a unique string for each message.
+       */
+      int count = 0;
+      while (ros::ok())
+      {
+        /**
+         * This is a message object. You stuff it with data, and then publish it.
+         */
+
+        CartConrolPlugin::Velocity v;
+
+       v.left = 10.0f;
+       v.right = 10.0f;
+       v.back = 10.0f;
+
+
+
+        std::cout << velocityToString(v) << "-----------\n";
+
+        /**
+         * The publish() function is how you send messages. The parameter
+         * is the message object. The type of this object must agree with the type
+         * given as a template parameter to the advertise<>() call, as was done
+         * in the constructor above.
+         */
+
+        chatter_pub.publish(v);
+
+
+        ros::spinOnce();
+
+
+        loop_rate.sleep();
+
+        ++count;
+      }
 
   return 0;
 }
-// %EndTag(FULLTEXT)%
+
